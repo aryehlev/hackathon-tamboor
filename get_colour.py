239f70ae8885dba6dde3c6b21ffa0783e86072d7
aryehlev <59808808+aryehlev@users.noreply.github.com>
2021-06-03 15:35:05 +0300

    As

--- get_colour.py
+++ get_colour.py
@@ -1,6 +1,6 @@
 import numpy as np
 from math import dist
-
+from functools import reduce
 
 
 def get_colour():
@@ -12,12 +12,17 @@ def distance(first_colour, second_colour):
     return dist(first_colour, second_colour)
 
 #1 / distance of two numbers(colours one channel)
-def w(first_colour, second_colour):
-    return 1 / abs(first_colour - second_colour)
+def w(colour, other_colour):
+    return 1 / abs(colour - other_colour)
+
+def sum_w_s(colour, array_of_other_colours):
+    
+    return np.sum(map(lambda a: w(colour, a),array_of_other_colours ))
+
 # def shepards_interpolation():
 
 #     pass
 
 
 if __name__ == '__main__':
-    print(distance([3],[4]))
+    print(sum_w_s([3],[4]))
